5cb2604e3c1963ecd065c776c9a43ba003fafd79
onze <slapexs@gmail.com>
2022-08-01 21:28:50 +0700

    Setting project

---
+++ backend/condb.php
@@ -0,0 +1,12 @@
+<?php
+$db_name = "tinbao";
+$db_password = "root";
+$db_username = "root";
+$db_host = "localhost";
+
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+if(!$conn){
+    die("Database connect failed: ". mysqli_connect_error());
+}else{
+    header('location: ../');
+}
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Metatag -->
+    <!-- Primary Meta Tags -->
+    <title>หิวน้ำรึเปล่า? - ถ้าหิวน้ำก็ต้องกินน้ำนะ</title>
+    <meta name="title" content="หิวน้ำรึเปล่า? - ถ้าหิวน้ำก็ต้องกินน้ำนะ">
+    <meta name="description" content="รู้หรือไม่ น้ำ คือส่วนประกอบหลักๆของร่างกายเลยนะถ้าหิวน้ำ ใช่แล้ว! ก็ต้องกินน้ำยังไงล่ะ">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://fakewaterdispenser.ddns.net">
+    <meta property="og:title" content="หิวน้ำรึเปล่า? - ถ้าหิวน้ำก็ต้องกินน้ำนะ">
+    <meta property="og:description" content="รู้หรือไม่ น้ำ คือส่วนประกอบหลักๆของร่างกายเลยนะถ้าหิวน้ำ ใช่แล้ว! ก็ต้องกินน้ำยังไงล่ะ">
+    <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="http://fakewaterdispenser.ddns.net">
+    <meta property="twitter:title" content="หิวน้ำรึเปล่า? - ถ้าหิวน้ำก็ต้องกินน้ำนะ">
+    <meta property="twitter:description" content="รู้หรือไม่ น้ำ คือส่วนประกอบหลักๆของร่างกายเลยนะถ้าหิวน้ำ ใช่แล้ว! ก็ต้องกินน้ำยังไงล่ะ">
+    <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+<!-- Script Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
