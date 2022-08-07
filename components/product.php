aaad201a59e7d06fe407f62251daeb1cbf3de65e
onze <slapexs@gmail.com>
2022-08-07 10:46:36 +0700

    Change display all products

--- components/product.php
+++ components/product.php
@@ -9,7 +9,7 @@ $qallpdt = $conn->query($allpdt);
 $productTypes_id = [];
 $productTypes_name = [];
 
-while($rallpdt = mysqli_fetch_array($qallpdt)){
+while ($rallpdt = mysqli_fetch_array($qallpdt)) {
     array_push($productTypes_id, $rallpdt['type_id']);
     array_push($productTypes_name, $rallpdt['type_name']);
 }
@@ -17,7 +17,7 @@ while($rallpdt = mysqli_fetch_array($qallpdt)){
 
 <div class="row mt-5">
     <!-- Add producttype -->
-    <div class="col-md-4">
+    <div class="col-md mx-auto">
         <div class="card">
             <div class="card-header">
                 <h5 class="card-title mb-0"><i class="fas fa-plus"></i> เพิ่มสินค้า</h5>
@@ -49,7 +49,7 @@ while($rallpdt = mysqli_fetch_array($qallpdt)){
                     <div class="form-group mt-2">
                         <select name="product_type" id="product_type" class="form-select" aria-label="Default producttype">
                             <option value="0" selected hidden disabled>-- เลือกประเภทสินค้า</option>
-                            <?php for ($i=0; $i<count($productTypes_id); $i++) { ?>
+                            <?php for ($i = 0; $i < count($productTypes_id); $i++) { ?>
                                 <option value="<?= $productTypes_id[$i]; ?>"><?= $productTypes_name[$i]; ?></option>
                             <?php } ?>
                         </select>
@@ -68,32 +68,30 @@ while($rallpdt = mysqli_fetch_array($qallpdt)){
             </div>
         </div>
     </div>
+</div>
 
-    <!-- List producttypes -->
-    <div class="col-md-8">
-        <h4><i class="fas fa-boxes-packing"></i> สินค้าทั้งหมด</h4>
-        <div class="table-responsive">
-            <table class="table-sm table table-bordered">
-                <thead class="table-dark">
-                    <tr>
-                        <th scope="col" class="text-center">ID</th>
-                        <th scope="col">ชื่อ</th>
-                        <th scope="col" class="text-center">จำนวนสินค้า</th>
-                        <th scope="col" class="text-center">ประเภทสินค้า</th>
-                    </tr>
-                </thead>
+<div class="mt-4">
+    <h4>All products</h4>
+    <hr>
 
-                <tbody>
-                    <?php while ($rallpd = mysqli_fetch_array($qallpd)) { ?>
-                        <tr>
-                            <td class="text-center"><?= $rallpd['product_id']; ?></td>
-                            <td><?= $rallpd['product_name']; ?></td>
-                            <td class="text-center"><?= $rallpd['product_amount']; ?></td>
-                            <td class="text-center"><?= $productTypes_name[$rallpd['product_type']-1]; ?></td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
+    <div class="row row-cols-2 row-cols-md-3 g-4">
+        <?php while ($rallpd = mysqli_fetch_array($qallpd)) { ?>
+            <div class="col">
+                <div class="card">
+                    <img src="./upload/<?= $rallpd['product_image']; ?>" alt="product image" class="img-card-top">
+                    <div class="card-body">
+                        <h5 class="card-title mb-0"><?= $rallpd['product_name']; ?></h5>
+                        <p class="card-text">
+                            <strong>ราคา: <?= $rallpd['product_price']; ?></strong><br>
+                            <strong>เหลือ: <?= number_format($rallpd['product_amount'], 0); ?></strong><br>
+                        </p>
+                    </div>
+
+                    <div class="card-footer">
+                        <small class="text-muted">หมวดหมู่: <?= $productTypes_name[$rallpd['product_type']-1]; ?></small>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
     </div>
 </div>
