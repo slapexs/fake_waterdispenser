e640a4d631eea34310377cbf7b437d6d2fb7883f
onze <slapexs@gmail.com>
2022-08-07 11:18:41 +0700

    Change display layout product type page and get data from database

--- components/main.php
+++ components/main.php
@@ -1,59 +1,20 @@
 <?php
-$pd_types = [
-    [
-        [
-            "img" => "https://cdn-icons-png.flaticon.com/512/2872/2872397.png",
-            "name" => "เครื่องดื่มขายดี",
-            "type" => "recommend"
-        ],
-        [
-            "img" => "https://cdn-icons-png.flaticon.com/512/590/590749.png",
-            "name" => "กาแฟ",
-            "type" => "coffee"
-        ]
-    ],
-    [
-        [
-            "img" => "https://cdn-icons-png.flaticon.com/512/5303/5303997.png",
-            "name" => "ชา",
-            "type" => "tea"
-        ],
-        [
-            "img" => "https://cdn-icons-png.flaticon.com/512/3081/3081162.png",
-            "name" => "นม โกโก้ และคาราเมล",
-            "type" => "smoothie"
-        ]
-    ],
-    [
-        [
-            "img" => "https://cdn-icons-png.flaticon.com/512/2405/2405479.png",
-            "name" => "น้ำอัดลม โซดา",
-            "type" => "soda"
-        ],
-        [
-            "img" => "https://cdn-icons-png.flaticon.com/512/2405/2405470.png",
-            "name" => "อื่น ๆ",
-            "type" => "other"
-        ]
-    ]
-];
+$product_type = "SELECT * FROM `product_types`";
+$qproduct_type = $conn->query($product_type);
 ?>
 
 <h1 class="text-center display-4 mb-5">กรุณาเลือกประเภทสินค้า</h1>
 <section class="row mx-auto">
-    <?php for ($i = 0; $i < 3; $i++) { ?>
-        <div class="col-md-6 text-center my-3">
-            <div class="p-4 border rounded border-2" id="<?= $pd_types[$i][0]["type"]; ?>">
-                <img src="<?= $pd_types[$i][0]["img"]; ?>" class="img-fluid mb-2" width="64" alt="เครื่องดื่มขายดี">
-                <h3><?= $pd_types[$i][0]["name"]; ?></h3>
+    <?php while ($rproduct_type = mysqli_fetch_array($qproduct_type)) { ?>
+        <a href="?page=pickproduct&type=<?= $rproduct_type['type_id']; ?>" class="col-md-6 text-center my-3 text-decoration-none text-dark">
+            <div>
+                <div class="p-4 border rounded border-2">
+                    <img src="<?= ($rproduct_type['type_link_icon'] != null ? $rproduct_type['type_link_icon'] : './static/image/noimage.png') ?>" alt="product type" class="img-fluid mb-2" width="64">
+                    <h3><?= $rproduct_type['type_name']; ?></h3>
+                </div>
             </div>
-        </div>
-
-        <div class="col-md-6 text-center my-3">
-            <div class="p-4 border rounded border-2" id="<?= $pd_types[$i][1]["type"]; ?>">
-                <img src="<?= $pd_types[$i][1]["img"]; ?>" class="img-fluid mb-2" width="64" alt="เครื่องดื่มขายดี">
-                <h3><?= $pd_types[$i][1]["name"]; ?></h3>
-            </div>
-        </div>
-    <?php } ?>
+        </a>
+    <?php
+        $couter++;
+    } ?>
 </section>
