556a13ca22e9ce933d134459fad4a924e7392ff5
onze <slapexs@gmail.com>
2022-08-14 16:18:58 +0700

    Function check order status in ordercompleted page

--- backend/order.php
+++ backend/order.php
@@ -4,14 +4,25 @@ include_once('./condb.php');
 // Place order
 if (isset($_POST['placeOrder'])) {
     $order = $_POST['placeOrder'];
-    
+
     $ins = "INSERT INTO `orders` (`order_id`,`order_product`,`order_amount`, `order_ref_machine`, `order_price`) VALUES ('" . $order[4] . "', '" . $order[0] . "','" . $order[1] . "','" . $order[2] . "','" . $order[3] . "')";
     $qins = $conn->query($ins);
-    if($qins){
+    if ($qins) {
         $res = 'inserted';
-    }else{
+    } else {
         $res = 'not_insert';
     }
     $response = ['status' => $res];
+    echo json_encode($response);
+}
+
+// Check order status
+if (isset($_POST['checkStatus'])) {
+    $orderId = $_POST['checkStatus'];
+    $chk = "SELECT * FROM `orders` WHERE `order_id` = '" . $orderId . "' ";
+    $qchk = $conn->query($chk);
+    $rchk = mysqli_fetch_assoc($qchk);
+    $orderStatus = $rchk['order_status'];
+    $response = ['status' => $orderStatus];
     echo json_encode($response);
 }
--- components/orderCompleted.php
+++ components/orderCompleted.php
@@ -1,4 +1,9 @@
+<?php
+$orderID = $_GET['orderId'];
+?>
+
 <div class="d-flex flex-column justify-content-center align-items-center vh-100">
+    <p id="defaultOrderId" class="d-none"><?= $orderID; ?></p>
     <img src="./static/image/shopping-cart.png" class="img-fluid" alt="Loading image" width="256">
     <h1 class="mt-5">
         <span class="display-2">กรุณารอรับเครื่องดื่ม</span>
@@ -7,3 +12,54 @@
         <span class="visually-hidden">Loading...</span>
     </div>
 </div>
+
+
+<script>
+    let isPaused = false
+    let time = 0
+    setInterval(() => {
+        if (!isPaused) {
+            time++
+            const defaultOrderId = document.querySelector('#defaultOrderId').innerHTML
+            $.ajax({
+                url: './backend/order.php',
+                type: 'post',
+                data: {
+                    checkStatus: defaultOrderId
+                },
+                dataType: 'json',
+                success: (res) => {
+                    if (res.status == 1) {
+                        isPaused = true
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'success',
+                            title: 'เรียบร้อย!',
+                            text: 'ขอบคุณที่อุดหนุน',
+                            timer: 3000,
+                            timerProgressBar: true,
+                            showConfirmButton: false,
+                            allowEscapeKey: false,
+                            allowOutsideClick: false
+                        })
+                    }
+                },
+                error: (err) => {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'error',
+                        title: 'เอ๊อะ!',
+                        text: 'มีบางอย่างผิดพลาด',
+                        showConfirmButton: true,
+                        allowOutsideClick: false,
+                        allowEscapeKey: false
+                    }).then((e) => {
+                        if (e.isConfirmed) {
+                            window.location.href = './'
+                        }
+                    })
+                }
+            })
+        }
+    }, 2000)
+</script>
